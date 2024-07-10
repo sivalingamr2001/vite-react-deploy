c61287c8df6baf25da8fde1d4e46bcd8e0d2bcb0
sivalingam <sivalinga1902@gmail.com>
2024-07-10 19:00:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,197 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import propTypes from "prop-types";
+import "./App.css";
+
+// Images
+import searchIcon from './assets/search.png';
+import rain from "./assets/gif/rain.gif";
+import sun from "./assets/gif/sun.gif";
+import cloudy from "./assets/gif/cloudy-day.gif";
+import lighting from "./assets/gif/cloud-lightning.gif";
+import snow from "./assets/gif/snow-storm.gif";
+import humidityIcon from "./assets/humidity.png";
+import windIcon from "./assets/wind.png";
+
+const WeatherDetails = ({
+  icon,
+  temp,
+  city,
+  country,
+  lat,
+  log,
+  humidity,
+  wind,
+}) => {
+  return (
+    <>
+      <div className="image">
+        <img src={icon} alt="image" />
+      </div>
+      <div className="temp">{temp}°C</div>
+      <div className="location">{city}</div>
+      <div className="country">{country}</div>
+      <div className="cord">
+        <div>
+          <span className="lat">Latitude</span>
+          <span>{lat}</span>
+        </div>
+        <div>
+          <span className="log">Longitude</span>
+          <span>{log}</span>
+        </div>
+      </div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidityIcon} alt="humidity" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">{humidity}%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={windIcon} alt="wind" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">{wind} km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+WeatherDetails.propTypes = {
+  icon: propTypes.string.isRequired,
+  temp: propTypes.number.isRequired,
+  city: propTypes.string.isRequired,
+  country: propTypes.string.isRequired,
+  humidity: propTypes.number.isRequired,
+  wind: propTypes.number.isRequired,
+  lat: propTypes.number.isRequired,
+  log: propTypes.number.isRequired,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  let api_key = "25124bd8f6ab47496ff6313fa0b8302a";
+  const [icon, setIcon] = useState(rain);
+  const [temp, setTemp] = useState(0);
+  const [text, setText] = useState("Chennai");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [lat, setlat] = useState(0);
+  const [log, setlog] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [wind, setWind] = useState(0);
+
+  const [cityNotFound, setCityNotFound] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const weatherIconMap = {
+    "01d": cloudy,
+    "01n": cloudy,
+    "02d": sun,
+    "02n": sun,
+    "03d": sun,
+    "03n": sun,
+    "04d": lighting,
+    "04n": lighting,
+    "09d": rain,
+    "09n": rain,
+    "10d": rain,
+    "10n": rain,
+    "11d": lighting,
+    "13d": snow,
+    "13n": snow,
+  };
+
+  const search = async () => {
+    setLoading(true);
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${text}&units=Metric&appid=${api_key}&units=Metric`;
+
+    try {
+      let res = await fetch(url);
+      let data = await res.json();
+      // console.log(data);
+      if (data.cod === "404") {
+        console.error("City Not Found");
+        setCityNotFound(true);
+        setLoading(false);
+        return;
+      }
+
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setTemp(Math.floor(data.main.temp));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setlat(data.coord.lat);
+      setlog(data.coord.lon);
+      const weatherIconCode = data.weather[0].icon;
+      setIcon(weatherIconMap[weatherIconCode] || sun);
+      setCityNotFound(false);
+    } catch (error) {
+      console.error("An error Occurred:", error.message);
+      setError("An error occurred while fetching weather data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCity = (e) => {
+    setText(e.target.value);
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  useEffect(function () {
+    search();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="container">
+        <div className="input-container">
+          <input
+            type="text"
+            className="City"
+            placeholder="Search city"
+            onChange={handleCity}
+            value={text}
+            onKeyDown={handleKeyDown}
+          />
+          <div className="search-icon" onClick={() => search()}>
+            <img src={searchIcon} alt="search" />
+          </div>
+        </div>
+
+        {loading && <div className="loading-message">Loading...</div>}
+        {error && <div className="error-message">{error}</div>}
+        {cityNotFound && <div className="city-not-found">City Not Found</div>}
+
+        {!loading && !cityNotFound && (
+          <WeatherDetails
+            icon={icon}
+            temp={temp}
+            city={city}
+            country={country}
+            lat={lat}
+            log={log}
+            humidity={humidity}
+            wind={wind}
+          />
+        )}
+
+        <p className="copyright">
+          Made by <span>Sivalingam R</span>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/Font/fotograami_hearts/readme.txt
@@ -0,0 +1,17 @@
+﻿السلام عليكم 
+
+هذا الخط من اعداد أخوكم فوتوغرامي 
+
+instagram & telegram
+@fotograami
+هذا الخط :
+* fotogrami-hearts01
+يوجد فيه ما يقارب 70 تشيكلة من القلوب
+
+لمتابعتنا 
+
+قناتنا في التليغرام :
+
+https://telegram.me/fotograami
+
+شكرا لكم 
